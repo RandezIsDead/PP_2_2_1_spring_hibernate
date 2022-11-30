bf777ca21646927ba131597e1f33b6c4d7aad00f
randez <hckrden@gmail.com>
2022-11-30 14:21:49 +1000

    Commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,32 +1,36 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
-import java.util.List;
+import javax.persistence.NoResultException;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
+   public static void main(String[] args) {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car nissan = new Car("Nissan", 12);
+      Car toyota = new Car("Toyota", 52);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      userService.add(new User("Shamil", "Bosayev", "eleven@mail.ru", nissan));
+      userService.add(new User("Jordan", "Bellford", "ws@mail.ru", toyota));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user.toString());
+      }
+
+      System.out.println(userService.getUserByCar("Toyota", 52));
+
+      try {
+         User notFoundUser = userService.getUserByCar("BELAZ", 34);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
       }
 
       context.close();
